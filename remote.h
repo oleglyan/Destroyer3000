c1781dc514bb2e79d12aa93e8f2ccc1f54496ac2
Oleg Lyan <oleglyan@gmail.com>
2014-05-08 13:55:25 +0300

    remote.h

---
+++ remote.h
@@ -0,0 +1,37 @@
+//SONY RMT-814 IR Remote Control codes
+#define RMT_814_FWD   0x2C9B //Forward, as button T
+#define RMT_814_BWD   0x6C9B //Backward, as button W
+#define RMT_814_POWER 0x4C9D //Turn off 0 as RED "Power" button
+#define RMT_814_RIGHT 0x6BC  //Turn right button >>|
+#define RMT_814_LEFT  0xEBC  //Turn left  button |<<
+#define RMT_814_ZERO  0xE3C  //Stop, as button ZERO
+
+//SONY RM-SR200 IR Remote Control codes
+#define RM_SR200_POWER 0xA81
+#define RM_SR200_STOP 0x1CB9C
+
+//EJ RC-5
+#define RC_5_SLEEP_1 0x1026
+#define RC_5_SLEEP_2 0x1826
+#define RC_5_POWER_1 0x100C
+#define RC_5_POWER_2 0x180C
+#define RC_5_1_1     0x1001
+#define RC_5_1_2     0x1801
+#define RC_5_2_1     0x1002
+#define RC_5_2_2     0x1802
+#define RC_5_3_1     0x1003
+#define RC_5_3_2     0x1803
+#define RC_5_4_1     0x1004
+#define RC_5_4_2     0x1804
+#define RC_5_5_1     0x1005
+#define RC_5_5_2     0x1805
+#define RC_5_6_1     0x1006
+#define RC_5_6_2     0x1806
+#define RC_5_7_1     0x1007
+#define RC_5_7_2     0x1807
+#define RC_5_8_1     0x1008
+#define RC_5_8_2     0x1808
+#define RC_5_9_1     0x1009
+#define RC_5_9_2     0x1809
+#define RC_5_0_1     0x1000
+#define RC_5_0_2     0x1800
